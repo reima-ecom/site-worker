45a40e5cca45c2270b5fde6e9c3b3db18967d021
Eric Selin <eric.selin@gmail.com>
2021-02-12 13:50:02 +0100

    fix: Add trailing slash removers to `mod.ts`

--- mod.ts
+++ mod.ts
@@ -2,4 +2,8 @@ export { getEventListener } from "./handler.ts";
 export { fetchFromHost } from "./asset-getters/proxy.ts";
 export { getRedirecter } from "./response-transformers/redirecter.ts";
 export { rewriteForTesting } from "./response-transformers/ab-tester.ts";
+export {
+  notFound404WhenTrailingSlash,
+  redirectIfTrailingSlash,
+} from "./response-transformers/trailing-slash-remover.ts";
 export type { Redirect } from "./response-transformers/redirecter.ts";
