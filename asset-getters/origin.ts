8b8225588b9702a44302b5ea5a21c15126d5c283
Eric Selin <eric.selin@gmail.com>
2021-02-25 17:01:01 +0100

    feat: Add origin fetcher that adds a trailing slash if dir

--- asset-getters/origin.ts
+++ asset-getters/origin.ts
@@ -7,3 +7,14 @@ import type { FetchEventHandler } from "../handler.ts";
 export const fetchFromOrigin: FetchEventHandler = async ({ request }) => {
   return fetch(request);
 };
+
+export const fetchFromOriginWithTrailingSlash: FetchEventHandler = async ({ request }) => {
+  // get url
+  const url = new URL(request.url);
+  // add trailing slash if directory
+  // (this in order to avoid a redirect e.g. `/folder` -> `/folder/`)
+  const lastPathSegment = url.pathname.split("/").pop();
+  if (lastPathSegment && !lastPathSegment.includes(".")) url.pathname += "/";
+  // fetch without headers or anything else from the original request
+  return fetch(url.toString(), request);
+};
